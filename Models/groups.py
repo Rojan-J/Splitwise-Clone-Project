4d293596332351364cdd9cb2ccc5b069f92ff0b3
Niloofar Torki <110784444+nilootorki@users.noreply.github.com>
2024-12-24 00:00:38 +0330

    changes in group classs

--- Models/groups.py
+++ Models/groups.py
@@ -4,19 +4,25 @@ from users import Users
 class Groups:
     def __init__(self, group_name):
         self.group_name = group_name
-        self.members = []
+        self.members = dict()
         self.expenses = []
-        self.debts = []
+        self.debts =dict()
 
     def add_members(self, name, email):
-        if Users(name, email) not in self.members:
-            self.members.append(Users(name, email))
+        if name not in self.members:
+            self.members[name] = email
 
     def add_expenses(self, expense, payer, contributers):
         self.expenses.append([expense, payer, contributers])
+        self.cal_debts()
 
     def cal_debts(self):
-        for expense in self.expenses:
-            portion = expense[0]/len(expense[2])
-            for contributer in expense[2]:
-                self.debts.append((contributer, expense[1], portion))
+        expense = self.expenses[-1]
+        portion = expense[0]/len(expense[2])
+        for contributer in expense[2]:
+            if (contributer, expense[1]) not in self.debts.keys():
+                self.debts[(contributer, expense[1])]= {"capacity": portion, "flow": 0}
+            else:
+                self.debts[(contributer, expense[1])]["capacity"] += portion
+
+
