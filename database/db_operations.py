9dfb691f9d28129a59a32893d162ac94bb4e0ad3
Rojan Jaleh <rojanjaleh22@gmail.com>
2024-12-25 22:35:19 -0800

    added relationship and debt management functions in dp operations

--- database/db_operations.py
+++ database/db_operations.py
@@ -48,6 +48,68 @@ def get_all_groups():
     connection.close()
     return groups
 
+def add_user_to_group(user_id,group_id):
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute('''
+        INSERT INTO user_group (user_id, group_id)
+        VALUES (?, ?)
+    ''', (user_id, group_id))
+    connection.commit()
+    connection.close()
+    
+def add_temp_user_tto_group(temp_user_id, group_id, temp_member_name, temp_member_email):
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute('''
+        INSERT INTO user_group (temp_user_id, group_id, temp_memmber_name, temp_member_email)
+        VALUES (?, ?, ?, ?)
+    ''', (temp_user_id, group_id, temp_member_name, temp_member_email))
+    connection.commit()
+    connection.close()
+    
+    
+    
+def add_expanse(group_id,payer_id,amount,category,date,deescription=None):
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute('''
+        INSERT INTO expenses (group_id, payer_id, amount, category, date, description)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', (group_id, payer_id, amount, category, date, description))
+    connection.commit()
+    connection.close()
+    
+    
+def add_contribution(expense_id,user_id,amont_contributed):
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute('''
+        INSERT INTO expense_user (expense_id, user_id, amount_contributed)
+        VALUES (?, ?, ?)
+    ''', (expense_id, user_id, amount_contributed))
+    connection.commit()
+    connection.close()
+    
+    
+def add_debt(debtor_id,creditor_id,amount):
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute('''
+        INSERT INTO debts (debtor_id, creditor_id, amount)
+        VALUES (?, ?, ?)
+    ''', (debtor_id, creditor_id, amount))
+    connection.commit()
+    connection.close()
+    
 
+def update_debt_status(debt_id,status):
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute('''
+        UPDATE debts SET status = ? WHERE debt_id = ?
+    ''', (status, debt_id))
+    connection.commit()
+    connection.close()
     
     
