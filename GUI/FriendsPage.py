a3096970ef53fba52e853cc58468bc86a80b292a
Rojan Jaleh <rojanjaleh22@gmail.com>
2025-01-03 12:04:17 -0800

    add new member function
    it gives error beacuse the error label3 is not generated completely

--- GUI/FriendsPage.py
+++ GUI/FriendsPage.py
@@ -6,6 +6,11 @@ import os
 import json
 sys.path.append(os.path.abspath("C:/Users/niloo/Term7/AP/Project/Splitwise-Clone-Project/database"))
 sys.path.append(os.path.abspath("C:/Users/niloo/Term7/AP/Project/Splitwise-Clone-Project/Models"))
+
+#sys.path.append(os.path.abspath(r"C:\Users\LENOVO\OneDrive\Documents\GitHub\Splitwise-Clone-Project\database"))
+#sys.path.append(os.path.abspath(r"C:\Users\LENOVO\OneDrive\Documents\GitHub\Splitwise-Clone-Project\Models"))
+
+
 from db_operations import *
 from friends import *
 import sqlite3
--- GUI/main_GUI.py
+++ GUI/main_GUI.py
@@ -18,16 +18,18 @@ from interface import Ui_MainWindow as InterfacePageUI
 
 
 import ctypes
-os.chdir("Project/Splitwise-Clone-Project/GUI")   #to change the current working directory
-#r"C:\Users\LENOVO\OneDrive\Documents\GitHub\Splitwise-Clone-Project\GUI"
+#os.chdir(r"C:\Users\LENOVO\OneDrive\Documents\GitHub\Splitwise-Clone-Project\GUI")   #to change the current working directory
 
+os.chdir("Project/Splitwise-Clone-Project/GUI")   #to change the current working directory
 
 sys.path.append(os.path.abspath("C:/Users/niloo/Term7/AP/Project/Splitwise-Clone-Project/database"))
-#r"C:\Users\LENOVO\OneDrive\Documents\GitHub\Splitwise-Clone-Project\database"
+
+
 from db_operations import add_user, get_user_by_email, add_group, get_all_groups, get_all_usernames
 
 # Explicitly load the cairo DLL
 cairo_path = r"C:/Program Files/GTK3-Runtime Win64/bin/libcairo-2.dll"
+
 ctypes.CDLL(cairo_path)
 #r"C:\Program Files\GTK3-Runtime Win64\bin\libcairo-2.dll"
 
@@ -242,7 +244,8 @@ class MainWindow(QMainWindow):
 
         self.ui.FriendsIcon.clicked.connect(lambda: show_all_existing_friends(self.ui, self.user))
         
-        
+        self.ui.AddMembersBtn_7.clicked.connect(lambda: add_new_member_to_group(self.ui, self.group))
+
     
 
         
